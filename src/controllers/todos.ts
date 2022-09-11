feaa737e78e812f5fc0aafa0037f1be2719ea658
Radu <raduadrian.constantin94@gmail.com>
2022-09-11 15:23:05 +0300

    Preparing to implement tests

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -4,13 +4,56 @@ const Todo = require('../models/todos');
 
 const todosRouter = require('express').Router();
 
+//Get all todos
 todosRouter.get('/', (request: Request, response: Response) => {
   Todo.find({}).then((todos: object) => {
     response.json(todos);
   })
   .catch((error: any) => {
-    error.log(`Error: ${error}`);
+    console.log(`Error: ${error}`);
   })
-})
+});
+
+//Get specific todo by id
+todosRouter.get('/:id', (request: Request, response: Response) => {
+  console.log(request.params.id)
+  Todo.findById(request.params.id).then((todo: object) => {
+    if (todo) {
+      response.json(todo);
+    } else {
+      response.status(404).end();
+    }
+  })
+  .catch((error: any) => {
+    console.log(`Error: ${error}`);
+  })
+});
+
+//Post a todo
+todosRouter.post('/', (request: Request, response: Response) => {
+  const body = request.body;
+
+  const todo = new Todo({
+    name: body.name,
+    details: body.details,
+  });
+
+  todo.save()
+    .then((savedTodo: object) => {
+      response.json(savedTodo)
+    })
+    .catch((error: any) => console.log(error))
+});
+
+//Delete a todo
+todosRouter.delete('/:id', (request: Request, response: Response) => {
+  Todo.findByIdAndRemove(request.params.id)
+    .then((todo: any) => { //Should I send back the deleted todo?!
+      response.status(204);
+    })
+    .catch((error: any) => console.log(error))
+});
+
+//Update a todo;
 
 module.exports = todosRouter;
--- src/models/todos.ts
+++ src/models/todos.ts
@@ -11,4 +11,12 @@ const todoSchema = new mongoose.Schema({
   }
 });
 
+todoSchema.set('toJSON', {
+  transform: (document: any, returnedObject: any) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 module.exports = mongoose.model('Todo', todoSchema);
