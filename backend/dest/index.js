11d852aef39a6904f773324114db7351eaf25820
Radu <raduadrian.constantin94@gmail.com>
2022-09-23 06:45:31 +0300

    App restrucutre. Adding frontent React

---
+++ backend/dest/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const config = require('./utils/config');
+const app = require('./app');
+const http = require('http');
+const server = http.createServer(app);
+server.listen(config.PORT, () => {
+    console.log(`Server is running on port ${config.PORT}`);
+});
---
+++ backend/dest/models/todos.js
@@ -0,0 +1,24 @@
+"use strict";
+const mongoose = require('mongoose');
+const todoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    details: {
+        type: String
+    },
+    done: {
+        type: Boolean,
+        default: false
+    }
+});
+todoSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+});
+module.exports = mongoose.model('Todo', todoSchema);
